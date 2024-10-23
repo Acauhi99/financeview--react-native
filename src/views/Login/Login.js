5d4c07d518004b38035a6fab42f767a3423eb524
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-22 23:10:24 -0300

    add: telas de auth

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,45 +1,72 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import { Formik } from "formik";
+import React, { useContext } from "react";
 import { TouchableOpacity, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
+import { UserContext } from "../../context/UserContext";
+import { LoginSchema } from "../../validation";
 import styles from "./Login.styles";
 
 const Login = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const { setUser } = useContext(UserContext);
 
-  const handleLogin = () => {
+  const handleLogin = (values) => {
     // Lógica de login aqui
-    console.log("Email:", email);
-    console.log("Password:", password);
+    console.log("Email:", values.email);
+    console.log("Password:", values.password);
+    setUser(values);
+    navigation.navigate("Dashboard");
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={styles.input}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        label="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        style={styles.input}
-        secureTextEntry
-      />
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <Text style={styles.link}>Criar Conta</Text>
-      </TouchableOpacity>
-      <Button mode="contained" onPress={handleLogin} style={styles.button}>
-        Login
-      </Button>
-    </View>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={LoginSchema}
+      onSubmit={handleLogin}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+      }) => (
+        <View style={styles.container}>
+          <Text style={styles.title}>Login</Text>
+          <TextInput
+            label="Email"
+            value={values.email}
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            style={styles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          {errors.email && touched.email ? (
+            <Text style={styles.error}>{errors.email}</Text>
+          ) : null}
+          <TextInput
+            label="Password"
+            value={values.password}
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            style={styles.input}
+            secureTextEntry
+          />
+          {errors.password && touched.password ? (
+            <Text style={styles.error}>{errors.password}</Text>
+          ) : null}
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text style={styles.link}>Criar Conta</Text>
+          </TouchableOpacity>
+          <Button mode="contained" onPress={handleSubmit} style={styles.button}>
+            Login
+          </Button>
+        </View>
+      )}
+    </Formik>
   );
 };
 
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,73 +1,148 @@
-import React, { useState } from "react";
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
+import React, { useContext, useState } from "react";
+import { TouchableOpacity, View } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Button, Text, TextInput } from "react-native-paper";
+import { UserContext } from "../../context/UserContext";
+import { RegisterSchema } from "../../validation";
 import styles from "./Register.styles";
 
 const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [dob, setDob] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const { setUser } = useContext(UserContext);
+  const navigation = useNavigation();
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-  const handleRegister = () => {
+  const handleRegister = (values) => {
     // Lógica de cadastro aqui
-    console.log("First Name:", firstName);
-    console.log("Last Name:", lastName);
-    console.log("Email:", email);
-    console.log("Date of Birth:", dob);
-    console.log("Password:", password);
-    console.log("Confirm Password:", confirmPassword);
+    setUser(values);
+    console.log("First Name:", values.firstName);
+    console.log("Last Name:", values.lastName);
+    console.log("Email:", values.email);
+    console.log("Date of Birth:", values.dob);
+    console.log("Password:", values.password);
+    console.log("Confirm Password:", values.confirmPassword);
+    navigation.navigate("Dashboard");
+  };
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date, setFieldValue) => {
+    setFieldValue("dob", date.toISOString().split("T")[0]);
+    hideDatePicker();
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Register</Text>
-      <TextInput
-        label="First Name"
-        value={firstName}
-        onChangeText={(text) => setFirstName(text)}
-        style={styles.input}
-      />
-      <TextInput
-        label="Last Name"
-        value={lastName}
-        onChangeText={(text) => setLastName(text)}
-        style={styles.input}
-      />
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        style={styles.input}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        label="Date of Birth"
-        value={dob}
-        onChangeText={(text) => setDob(text)}
-        style={styles.input}
-      />
-      <TextInput
-        label="Password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        style={styles.input}
-        secureTextEntry
-      />
-      <TextInput
-        label="Confirm Password"
-        value={confirmPassword}
-        onChangeText={(text) => setConfirmPassword(text)}
-        style={styles.input}
-        secureTextEntry
-      />
-      <Button mode="contained" onPress={handleRegister} style={styles.button}>
-        Register
-      </Button>
-    </View>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        dob: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={RegisterSchema}
+      onSubmit={handleRegister}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        setFieldValue,
+        values,
+        errors,
+        touched,
+      }) => (
+        <View style={styles.container}>
+          <Text style={styles.title}>Register</Text>
+          <TextInput
+            label="First Name"
+            value={values.firstName}
+            onChangeText={handleChange("firstName")}
+            onBlur={handleBlur("firstName")}
+            style={styles.input}
+          />
+          {errors.firstName && touched.firstName ? (
+            <Text style={styles.error}>{errors.firstName}</Text>
+          ) : null}
+          <TextInput
+            label="Last Name"
+            value={values.lastName}
+            onChangeText={handleChange("lastName")}
+            onBlur={handleBlur("lastName")}
+            style={styles.input}
+          />
+          {errors.lastName && touched.lastName ? (
+            <Text style={styles.error}>{errors.lastName}</Text>
+          ) : null}
+          <TextInput
+            label="Email"
+            value={values.email}
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            style={styles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          {errors.email && touched.email ? (
+            <Text style={styles.error}>{errors.email}</Text>
+          ) : null}
+          <View style={styles.input}>
+            <TouchableOpacity onPress={showDatePicker}>
+              <TextInput
+                label="Date of Birth"
+                value={values.dob}
+                onChangeText={handleChange("dob")}
+                onBlur={handleBlur("dob")}
+                editable={false}
+                pointerEvents="none"
+              />
+            </TouchableOpacity>
+          </View>
+          {errors.dob && touched.dob ? (
+            <Text style={styles.error}>{errors.dob}</Text>
+          ) : null}
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={(date) => handleConfirm(date, setFieldValue)}
+            onCancel={hideDatePicker}
+          />
+          <TextInput
+            label="Password"
+            value={values.password}
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            style={styles.input}
+            secureTextEntry
+          />
+          {errors.password && touched.password ? (
+            <Text style={styles.error}>{errors.password}</Text>
+          ) : null}
+          <TextInput
+            label="Confirm Password"
+            value={values.confirmPassword}
+            onChangeText={handleChange("confirmPassword")}
+            onBlur={handleBlur("confirmPassword")}
+            style={styles.input}
+            secureTextEntry
+          />
+          {errors.confirmPassword && touched.confirmPassword ? (
+            <Text style={styles.error}>{errors.confirmPassword}</Text>
+          ) : null}
+          <Button mode="contained" onPress={handleSubmit} style={styles.button}>
+            Register
+          </Button>
+        </View>
+      )}
+    </Formik>
   );
 };
 
