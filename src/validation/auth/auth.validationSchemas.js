797844b5d508aec6c12ef8df66128f812cbf2212
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-22 23:09:27 -0300

    add: validacao de formulario

---
+++ src/validation/auth/auth.validationSchemas.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+export const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().min(6, "Too Short!").required("Required"),
+});
+
+export const RegisterSchema = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  dob: Yup.date().required("Required"),
+  password: Yup.string().min(6, "Too Short!").required("Required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
+});
