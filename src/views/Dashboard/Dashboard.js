c20746d4231337d1009970f61e4d82a10e4583d6
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-22 23:09:57 -0300

    add: dashboard basica

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Text, View } from "react-native";
+import { UserContext } from "../../context/UserContext";
+import styles from "./Dashboard.styles";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user } = useContext(UserContext);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, {user?.firstName}!</Text>
+    </View>
+  );
 };
 
 export default Dashboard;
