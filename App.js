20ab0fe1934ec60fda9d80e2e2889c0f239c2830
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-25 23:38:08 -0300

    add: adicionando o toast na aplicacao

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Toast from "react-native-toast-message";
 import StackNavigator from "./src/components/StackNavigator";
 import { UserProvider } from "./src/context/UserContext";
+import { toastConfig } from "./src/utils";
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
         <StackNavigator />
         <StatusBar style="auto" />
       </NavigationContainer>
+      <Toast config={toastConfig} />
     </UserProvider>
   );
 }
