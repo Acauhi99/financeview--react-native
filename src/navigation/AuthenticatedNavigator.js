0bc03b261dcde74beea98de00e36a145b2108c7e
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-21 22:41:58 -0300

    refactor: adiciona blur

--- src/navigation/AuthenticatedNavigator.js
+++ src/navigation/AuthenticatedNavigator.js
@@ -19,6 +19,7 @@ export default function AuthenticatedNavigator() {
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
+        unmountOnBlur: true,
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
